0a8dc3ffcc055a62bf5611f9696fb4c90e49854c
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-27 00:37:28 +0200

    User create task, User view task

--- Tasks/Task1/src/cts/task1/models/user/User.java
+++ Tasks/Task1/src/cts/task1/models/user/User.java
@@ -1,10 +1,16 @@
 package cts.task1.models.user;
 
+import cts.task1.interfaces.ITaskViewable;
 import cts.task1.interfaces.IUser;
 import cts.task1.models.Task;
+import cts.task1.models.TaskManager;
 
-public abstract class User implements IUser {
+import java.util.List;
+
+public abstract class User implements IUser, ITaskViewable {
     private String name;
+
+    private TaskManager taskManager = TaskManager.getInstance();
 
     public User(String name) {
         this.name = name;
@@ -12,6 +18,16 @@ public abstract class User implements IUser {
 
     @Override
     public Task createTask(String name, String description, Boolean isVisible) {
-        return new Task(name, description, isVisible);
+        Task task = new Task(name, description, isVisible);
+        taskManager.addTask(task);
+        return task;
+    }
+
+    @Override
+    public void viewTasks() {
+        List<Task> visibleTasks = taskManager.getTasks().stream().filter((task -> task.getVisible() == true)).toList();
+        for(Task task : visibleTasks) {
+            System.out.println(task);
+        }
     }
 }
