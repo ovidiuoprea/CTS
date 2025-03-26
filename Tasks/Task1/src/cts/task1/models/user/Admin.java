1e23d61f4326250eb199d37d577458695bac0bd7
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-26 23:58:29 +0200

    Create regular & admin user

---
+++ Tasks/Task1/src/cts/task1/models/user/Admin.java
@@ -0,0 +1,7 @@
+package cts.task1.models.user;
+
+public class Admin extends User {
+    public Admin(String name) {
+        super(name);
+    }
+}
---
+++ Tasks/Task1/src/cts/task1/models/user/RegularUser.java
@@ -0,0 +1,7 @@
+package cts.task1.models.user;
+
+public class RegularUser extends User {
+    public RegularUser(String name) {
+        super(name);
+    }
+}
