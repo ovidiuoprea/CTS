26d1414aad408e6d956f7532d803adb2a7cd41a1
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-27 00:37:36 +0200

    Test create & view task in main

--- Tasks/Task1/src/Main.java
+++ Tasks/Task1/src/Main.java
@@ -1,5 +1,23 @@
+import cts.task1.models.Task;
+import cts.task1.models.user.Admin;
+import cts.task1.models.user.RegularUser;
+import cts.task1.models.user.User;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("CTS Lab - Task 1");
+
+        User admin = new Admin("Admin");
+        User regular = new RegularUser("Regular user");
+
+        Task task1 = regular.createTask("Task1", "First task created", false);
+        Task task2 = admin.createTask("Task2", "Second task created", true);
+        Task task3 = admin.createTask("Task3", "Third task created", false);
+
+        System.out.println("\n\nAdmin view tasks:\n");
+        admin.viewTasks();
+
+        System.out.println("\n\nRegular view tasks:\n");
+        regular.viewTasks();
     }
 }
