a21f2170e46f43333d868183f41cceb4566d8061
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-29 13:11:01 +0200

    Create concrete class Kebap

---
+++ Tasks/Task3/src/cts/task3/models/Kebap.java
@@ -0,0 +1,27 @@
+package cts.task3.models;
+
+import cts.task3.models.ingredients.carbohydrates.Carbohydrate;
+import cts.task3.models.ingredients.fibers.Fiber;
+import cts.task3.models.ingredients.healthy.Healthy;
+import cts.task3.models.ingredients.pickles.Pickle;
+import cts.task3.models.ingredients.proteins.Protein;
+import cts.task3.models.ingredients.sauces.Sauce;
+import cts.task3.models.ingredients.wraps.Wrap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kebap {
+    private Protein protein;
+    private Carbohydrate carbohydrate;
+    private List<Sauce> sauces = new ArrayList<>();
+    private Pickle pickle;
+    private Wrap wrap;
+    private Fiber fiber;
+    private Healthy healthy;
+
+    public Kebap(Protein protein, Carbohydrate carbohydrate) {
+        this.protein = protein;
+        this.carbohydrate = carbohydrate;
+    }
+}
