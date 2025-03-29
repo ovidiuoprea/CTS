51aecfda43def95948d52a28f9ad801c3d97d41f
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-29 16:06:07 +0200

    Updated sauce serializer to save sauce type, implemented deserializing

--- Tasks/Task3/src/cts/task3/serializers/SauceSerializer.java
+++ Tasks/Task3/src/cts/task3/serializers/SauceSerializer.java
@@ -2,11 +2,17 @@ package cts.task3.serializers;
 
 import cts.task3.models.ingredients.Ingredient;
 import cts.task3.models.ingredients.sauces.FermentingSauce;
+import cts.task3.models.ingredients.sauces.NonFermentingSauce;
 import cts.task3.models.ingredients.sauces.Sauce;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class SauceSerializer {
     public static void write(List<Sauce> sauces, String filepath) {
@@ -14,6 +20,9 @@ public class SauceSerializer {
             FileWriter fileWriter = new FileWriter(filepath);
             for(Sauce sauce : sauces) {
                 StringBuilder stringBuilder = new StringBuilder();
+
+                String sauceType = sauce instanceof FermentingSauce ? "fermenting" : "non-fermenting";
+                stringBuilder.append(sauceType).append(",");
 
                 stringBuilder.append(sauce.getCalories() + ",");
                 stringBuilder.append(sauce.getName() + ",");
@@ -32,5 +41,38 @@ public class SauceSerializer {
             System.out.println(e.getMessage());
         }
 
+    }
+
+    public static List<Sauce> read(String filepath) {
+        List<Sauce> sauces = new ArrayList<>();
+        try {
+            File file = new File(filepath);
+            Scanner fileReader = new Scanner(file);
+            while(fileReader.hasNextLine()) {
+                String data = fileReader.nextLine();
+                String[] splitData = data.split(",");
+
+                String sauceType = splitData[0];
+
+                Integer calories = Integer.parseInt(splitData[1]);
+                String name = splitData[2];
+                Boolean isSpicy = Boolean.parseBoolean(splitData[3]);
+
+                if(sauceType.equalsIgnoreCase("non-fermenting")) {
+                    Sauce sauce = new NonFermentingSauce(calories, name, isSpicy);
+                    sauces.add(sauce);
+                }
+                else {
+                    Integer expiryHours = Integer.parseInt(splitData[4]);
+                    Sauce sauce = new FermentingSauce(calories, name, isSpicy, expiryHours);
+                    sauces.add(sauce);
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return sauces;
     }
 }
