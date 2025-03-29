74cbec972bb41c613a8a32417016740d85e37764
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-29 13:02:51 +0200

    Implemented concrete classes for fibers: Cabbage, Tomato, Carrot

---
+++ Tasks/Task3/src/cts/task3/models/ingredients/fibers/Cabbage.java
@@ -0,0 +1,10 @@
+package cts.task3.models.ingredients.fibers;
+
+public class Cabbage extends Fiber {
+    private static final Integer CABBAGE_CALORIES = 22;
+    private static final Integer CABBAGE_TOTAL_FIBERS = 3;
+
+    public Cabbage() {
+        super(CABBAGE_CALORIES, CABBAGE_TOTAL_FIBERS);
+    }
+}
---
+++ Tasks/Task3/src/cts/task3/models/ingredients/fibers/Tomato.java
@@ -0,0 +1,10 @@
+package cts.task3.models.ingredients.fibers;
+
+public class Tomato extends Fiber {
+    private static final Integer TOMATO_CALORIES = 18;
+    private static final Integer TOMATO_TOTAL_FIBERS = 2;
+
+    public Tomato() {
+        super(TOMATO_CALORIES, TOMATO_TOTAL_FIBERS);
+    }
+}
