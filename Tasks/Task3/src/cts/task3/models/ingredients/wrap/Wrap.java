cdf2886ba70fa60f917c6043ff8edb517a591f6c
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-29 12:56:54 +0200

    Implemented abstract class Wrap

---
+++ Tasks/Task3/src/cts/task3/models/ingredients/wrap/Wrap.java
@@ -0,0 +1,13 @@
+package cts.task3.models.ingredients.wrap;
+
+import cts.task3.models.ingredients.Ingredient;
+
+public abstract class Wrap extends Ingredient {
+    // If a customer requests flatbread as a wrap, it's disappointing for the business owners
+    protected Boolean isDisappointing;
+
+    public Wrap(Integer calories, Boolean isDisappointing) {
+        super(calories);
+        this.isDisappointing = isDisappointing;
+    }
+}
