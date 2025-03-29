bceae008425a5ef7dd5527db1f22072dbbbe4022
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-29 19:54:29 +0200

    Removed magic numbers from Kebap Menu handleUserInput()

---
+++ Tasks/Task3/src/cts/task3/models/constants/KebapMenuOptionValues.java
@@ -0,0 +1,9 @@
+package cts.task3.models.constants;
+
+public class KebapMenuOptionValues {
+    public static final int CREATE = 1;
+    public static final int DELETE = 2;
+    public static final int SHOW_ALL = 3;
+    public static final int FILTER = 4;
+    public static final int BACK = 0;
+}
--- Tasks/Task3/src/cts/task3/models/menus/KebapMenu.java
+++ Tasks/Task3/src/cts/task3/models/menus/KebapMenu.java
@@ -1,6 +1,8 @@
 package cts.task3.models.menus;
 
 import cts.task3.interfaces.IMenu;
+import cts.task3.models.Kebap;
+import cts.task3.models.constants.KebapMenuOptionValues;
 
 import java.util.List;
 
@@ -17,11 +19,23 @@ public class KebapMenu extends Menu implements IMenu {
     @Override
     public void handleUserInput(Integer userChoice) {
         switch(userChoice) {
+            case KebapMenuOptionValues.CREATE:
+                // TODO
+                break;
+            case KebapMenuOptionValues.DELETE:
+                // TODO
+                break;
+            case KebapMenuOptionValues.SHOW_ALL:
+                // TODO
+                break;
+            case KebapMenuOptionValues.FILTER:
+                // TODO
+                break;
+            case KebapMenuOptionValues.BACK:
+                parentMenu.show();
+                break;
             default:
                 this.show();
-                break;
-            case 0:
-                parentMenu.show();
                 break;
         }
     }
