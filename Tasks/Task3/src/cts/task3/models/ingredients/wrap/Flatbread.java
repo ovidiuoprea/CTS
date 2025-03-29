2278a596623a295ae25d035d5749bdc24be802e7
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-29 12:57:06 +0200

    Implemented concrete classes for wraps: Flatbread, Salad

---
+++ Tasks/Task3/src/cts/task3/models/ingredients/wrap/Flatbread.java
@@ -0,0 +1,10 @@
+package cts.task3.models.ingredients.wrap;
+
+public class Flatbread extends Wrap{
+    private static final Integer FLATBREAD_CALORIES = 234;
+    private static final Boolean IS_FLATBREAD_DISAPPOINTING = Boolean.TRUE;
+
+    public Flatbread() {
+        super(FLATBREAD_CALORIES, IS_FLATBREAD_DISAPPOINTING);
+    }
+}
