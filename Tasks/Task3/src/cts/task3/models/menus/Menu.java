bcb001fd50e450b141834f9275dbb7430659f342
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-03-29 16:51:56 +0200

    Created Menu abstract class

---
+++ Tasks/Task3/src/cts/task3/models/menus/Menu.java
@@ -0,0 +1,40 @@
+package cts.task3.models.menus;
+
+import cts.task3.interfaces.IMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public abstract class Menu implements IMenu {
+    protected List<String> options;
+    public static List<String> MAIN_MENU_OPTIONS = Stream.of(
+            "Kebap menu",
+            "Sauce menu"
+    ).toList();
+
+    public Menu(List<String> options) {
+        this.options = options;
+    }
+
+    @Override
+    public void show() {
+        System.out.println("Welcome to \"Kebap nu cu de toate... ci doar cu ce trebuie... SRL\". How can we help?");
+        for(String option: options) {
+            System.out.println(
+                (options.indexOf(option) + 1) +
+                ". " +
+                option
+            );
+        }
+        System.out.println("Choose one: ");
+        Scanner in = new Scanner(System.in);
+        Integer choice = in.nextInt();
+        handleUserInput(choice);
+    }
+
+    @Override
+    public void handleUserInput(Integer userChoice) {
+    }
+}
