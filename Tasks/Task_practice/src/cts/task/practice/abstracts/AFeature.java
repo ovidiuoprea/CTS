64e42986e78f1b0212259d33d7c15c08dbf7d224
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-05 14:17:14 +0300

    Implemented AFeature constructor

--- Tasks/Task_practice/src/cts/task/practice/abstracts/AFeature.java
+++ Tasks/Task_practice/src/cts/task/practice/abstracts/AFeature.java
@@ -3,4 +3,9 @@ package cts.task.practice.abstracts;
 public abstract class AFeature {
     String name;
     int price;
+
+    AFeature(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
