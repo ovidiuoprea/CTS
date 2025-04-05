e6a4c3da2522fb23be07db66c1b54cc96474591e
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-05 15:40:40 +0300

    Tested creating a phone with factory method features

--- Tasks/Task_practice/src/Main.java
+++ Tasks/Task_practice/src/Main.java
@@ -1,7 +1,9 @@
 import cts.task.practice.Features;
 import cts.task.practice.abstracts.AFeature;
+import cts.task.practice.models.factory.feature.method.FeatureFactoryMethod;
 import cts.task.practice.models.factory.feature.simple.FeatureFactory;
 import cts.task.practice.models.Phone;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -22,5 +24,21 @@ public class Main {
         );
         System.out.println(phone);
 
+        FeatureFactoryMethod factoryMethod = new FeatureFactoryMethod();
+        AFeature methodAntenna = factoryMethod.getFeature(Features.ANTENNA, "Method antenna", 10);
+        AFeature methodBattery = factoryMethod.getFeature(Features.BATTERY, "Method battery", 20);
+        AFeature methodCompass = factoryMethod.getFeature(Features.COMPASS, "Method compass", 30);
+        AFeature methodSpeaker = factoryMethod.getFeature(Features.SPEAKER, "Method speaker", 40);
+
+        Phone phone2 = new Phone(
+                "Phone with factory method features",
+                "0.1.1-alpha",
+                methodAntenna,
+                methodBattery,
+                methodCompass,
+                methodSpeaker
+        );
+
+        System.out.println(phone2);
     }
 }
