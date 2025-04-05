70d176239d81fa397f19a7598d2177814adeed1a
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-05 15:18:57 +0300

    Tested creating a phone with simple factory features

--- Tasks/Task_practice/src/Main.java
+++ Tasks/Task_practice/src/Main.java
@@ -1,5 +1,26 @@
+import cts.task.practice.Features;
+import cts.task.practice.abstracts.AFeature;
+import cts.task.practice.models.factory.feature.simple.FeatureFactory;
+import cts.task.practice.models.Phone;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FeatureFactory simpleFactory = new FeatureFactory();
+
+        AFeature simpleAntenna = simpleFactory.getFeature(Features.ANTENNA, "Simple antenna", 10);
+        AFeature simpleBattery = simpleFactory.getFeature(Features.BATTERY, "Simple battery", 20);
+        AFeature simpleCompass = simpleFactory.getFeature(Features.COMPASS, "Simple compass", 30);
+        AFeature simpleSpeaker = simpleFactory.getFeature(Features.SPEAKER, "Simple speaker", 40);
+
+        Phone phone = new Phone(
+                "Phone with simple factory features",
+                "0.1.0-alpha",
+                simpleAntenna,
+                simpleBattery,
+                simpleCompass,
+                simpleSpeaker
+        );
+        System.out.println(phone);
+
     }
 }
