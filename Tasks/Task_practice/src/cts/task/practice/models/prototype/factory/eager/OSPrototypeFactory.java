9272e029bdf71c98148956fb96f26c567d23cbf7
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-05 19:55:07 +0300

    OS Prototype Factory Eager

---
+++ Tasks/Task_practice/src/cts/task/practice/models/prototype/factory/eager/OSPrototypeFactory.java
@@ -0,0 +1,34 @@
+package cts.task.practice.models.prototype.factory.eager;
+
+import cts.task.practice.exceptions.OSVersionNotImplementedException;
+import cts.task.practice.models.prototype.OS;
+
+import java.util.HashMap;
+
+public class OSPrototypeFactory {
+    private static HashMap<Integer, OS> OSVersions = new HashMap<>();
+
+    static {
+        try {
+            OSVersions.put(1, new OS(1));
+            OSVersions.put(2, new OS(2));
+        }
+        catch (Exception exception) {
+            System.out.println("Could not create OS due to: " + exception.getMessage());
+        }
+    }
+
+    // Propagates CloneNotSupportedException to the caller, ensuring all code paths either return a valid OS or throw an exception
+    public static OS getOS(int version) throws CloneNotSupportedException, OSVersionNotImplementedException {
+        switch (version) {
+            case 1:
+                return (OS) OSVersions.get(1).clone();
+            case 2:
+                return (OS) OSVersions.get(2).clone();
+            default:
+                throw new OSVersionNotImplementedException();
+        }
+    }
+
+    //TODO: Implement lazy Prototype Factory
+}
