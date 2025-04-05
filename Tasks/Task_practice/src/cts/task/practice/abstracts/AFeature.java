a9d4b9835df9677b62798209d665cec366b56fdc
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-05 14:19:10 +0300

    Updated AFeature constructor to be public

--- Tasks/Task_practice/src/cts/task/practice/abstracts/AFeature.java
+++ Tasks/Task_practice/src/cts/task/practice/abstracts/AFeature.java
@@ -4,7 +4,7 @@ public abstract class AFeature {
     String name;
     int price;
 
-    AFeature(String name, int price) {
+    public AFeature(String name, int price) {
         this.name = name;
         this.price = price;
     }
