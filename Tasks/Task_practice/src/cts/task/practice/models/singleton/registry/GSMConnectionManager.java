53bd794aa01a5fa0cc461b6bbc51515b332f1d64
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-05 18:26:03 +0300

    Created GSMConnection manager singleton registry

---
+++ Tasks/Task_practice/src/cts/task/practice/models/singleton/registry/GSMConnectionManager.java
@@ -0,0 +1,30 @@
+package cts.task.practice.models.singleton.registry;
+
+import cts.task.practice.enums.CallType;
+import cts.task.practice.interfaces.IGSMConnection;
+
+import java.util.HashMap;
+
+public class GSMConnectionManager implements IGSMConnection {
+    private int activeCalls;
+
+    private static HashMap<CallType, GSMConnectionManager> instances = new HashMap<>();
+
+    private GSMConnectionManager() {
+        this.activeCalls = 0;
+    }
+
+    public static GSMConnectionManager getInstance(CallType callType) {
+        GSMConnectionManager instance = instances.get(callType);
+        if(instance == null) {
+            instance = new GSMConnectionManager();
+            instances.put(callType, instance);
+        }
+        return instance;
+    }
+
+    @Override
+    public void call() {
+        this.activeCalls++;
+    }
+}
