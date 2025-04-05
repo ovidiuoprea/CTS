ea316a5fefe5b217ed42b04d51553a656d2573ae
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-05 15:18:45 +0300

    Added features to phone concrete class

--- Tasks/Task_practice/src/cts/task/practice/models/Phone.java
+++ Tasks/Task_practice/src/cts/task/practice/models/Phone.java
@@ -1,9 +1,33 @@
 package cts.task.practice.models;
 import cts.task.practice.abstracts.AFeature;
+import cts.task.practice.models.factory.feature.simple.FeatureFactory;
 
 public class Phone {
     String identifier;
     String version;
+    AFeature antenna;
     AFeature battery; // example
-    // add the features/add-ons
+    AFeature compass;
+    AFeature speaker;
+
+    public Phone(String identifier, String version, AFeature antenna, AFeature battery, AFeature compass, AFeature speaker) {
+        this.identifier = identifier;
+        this.version = version;
+        this.antenna = antenna;
+        this.battery = battery;
+        this.compass = compass;
+        this.speaker = speaker;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "identifier='" + identifier + '\'' +
+                ", version='" + version + '\'' +
+                ", antenna=" + antenna +
+                ", battery=" + battery +
+                ", compass=" + compass +
+                ", speaker=" + speaker +
+                '}';
+    }
 }
