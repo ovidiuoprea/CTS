4e19de8f7d1fa1d45273a30aa491fa8145e4dd56
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-05 14:21:09 +0300

    Implemented Battery feature concrete class

---
+++ Tasks/Task_practice/src/cts/task/practice/models/features/Battery.java
@@ -0,0 +1,11 @@
+package cts.task.practice.models.features;
+import cts.task.practice.abstracts.AFeature;
+
+public class Battery extends AFeature{
+    private int capacity;
+
+    public Battery(String name, int price) {
+        super(name, price);
+        this.capacity = 1000;
+    }
+}
