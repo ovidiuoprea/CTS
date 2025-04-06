3815b8a8c757bac61e21b580027e3a2fb0b8cd60
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-06 10:42:39 +0300

    Added fire hazard level to AFloor

--- Subjects/Subject2/src/cts/subject2/models/floor/AFloor.java
+++ Subjects/Subject2/src/cts/subject2/models/floor/AFloor.java
@@ -2,8 +2,19 @@ package cts.subject2.models.floor;
 
 public abstract class AFloor {
     protected int hardness;
+    protected int fireHazardLevel;
 
-    public AFloor(int hardness) {
-        this.hardness = hardness;
+    public static final int DEFAULT_FLOOR_HARDNESS = 5;
+    public static final int DEFAULT_FLOOR_FIRE_HAZARD_RATING = 2;
+
+    public AFloor() {
+    }
+
+    public int getFireHazardLevel() {
+        return fireHazardLevel;
+    }
+
+    public int getHardness() {
+        return hardness;
     }
 }
