c88a4e84211b23dd6f371ffa8b67c85764091654
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-06 10:50:25 +0300

    Created concrete decoration classes

---
+++ Subjects/Subject2/src/cts/subject2/models/decoration/GlassDecoration.java
@@ -0,0 +1,8 @@
+package cts.subject2.models.decoration;
+
+public class GlassDecoration extends ADecoration{
+    public GlassDecoration(String name) {
+        super(name);
+        this.fireHazardLevel = 2;
+    }
+}
---
+++ Subjects/Subject2/src/cts/subject2/models/decoration/PlasticDecoration.java
@@ -0,0 +1,8 @@
+package cts.subject2.models.decoration;
+
+public class PlasticDecoration extends ADecoration{
+    public PlasticDecoration(String name) {
+        super(name);
+        this.fireHazardLevel = 7;
+    }
+}
