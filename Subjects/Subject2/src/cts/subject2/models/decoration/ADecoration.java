71e1fed7289f47ed3ffc658e50315f3099aa3edc
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-06 10:50:17 +0300

    Created ADecoration abstract class

---
+++ Subjects/Subject2/src/cts/subject2/models/decoration/ADecoration.java
@@ -0,0 +1,10 @@
+package cts.subject2.models.decoration;
+
+public abstract class ADecoration {
+    protected String name;
+    protected int fireHazardLevel;
+
+    public ADecoration(String name) {
+        this.name = name;
+    }
+}
