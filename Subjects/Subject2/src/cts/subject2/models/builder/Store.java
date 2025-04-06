8b574f09755efeeb0fe7b9fc51fc465ad395990c
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-06 12:33:26 +0300

    Created Store concrete class

---
+++ Subjects/Subject2/src/cts/subject2/models/builder/Store.java
@@ -0,0 +1,45 @@
+package cts.subject2.models.builder;
+
+import cts.subject2.models.decoration.ADecoration;
+import cts.subject2.models.floor.AFloor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    String name;
+    float area;
+    int numberOfEntrances;
+    AFloor floor;
+    List<ADecoration> decorations;
+
+    Store(String name, float area, int numberOfEntrances) {
+        this.name = name;
+        this.area = area;
+        this.numberOfEntrances = numberOfEntrances;
+        this.decorations = new ArrayList<>();
+    }
+
+    void setFloor(AFloor floor) {
+        this.floor = floor;
+    }
+
+    void setNumberOfEntrances(int numberOfEntrances) {
+        this.numberOfEntrances = numberOfEntrances;
+    }
+
+    void addDecoration(ADecoration decoration) {
+        decorations.add(decoration);
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "name='" + name + '\'' +
+                ", area=" + area +
+                ", numberOfEntrances=" + numberOfEntrances +
+                ", floor=" + floor +
+                ", decorations=" + decorations +
+                '}';
+    }
+}
