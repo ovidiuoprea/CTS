a98c96dd93f7e2a5284282c18ee8d55ebc28cbc4
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-07 19:26:52 +0300

    Implemented concrete classes for each player type

---
+++ Subjects/Subject3/src/cts/subject3/models/player/Goalkeeper.java
@@ -0,0 +1,23 @@
+package cts.subject3.models.player;
+
+import cts.subject3.abstracts.APlayer;
+import cts.subject3.models.ProhibitedMedication;
+import cts.subject3.models.Training;
+
+import java.util.List;
+
+public class Goalkeeper extends APlayer {
+    Goalkeeper(String name, int age, List<Training> trainingSessions) {
+        super(name, age, trainingSessions);
+    }
+
+    @Override
+    public void addProhibitedMedication(ProhibitedMedication prohibitedMedication) {
+        super.addProhibitedMedication(prohibitedMedication);
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
