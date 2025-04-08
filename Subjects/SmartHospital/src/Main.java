ae417527db1d23e9fa4b6ba9d9a943ddf1e1e629
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-08 15:17:00 +0300

    SmartHospital - Tested simple factory

--- Subjects/SmartHospital/src/Main.java
+++ Subjects/SmartHospital/src/Main.java
@@ -1,5 +1,21 @@
+import enums.MedicalDeviceType;
+import models.medicaldevices.BloodAnalyzer;
+import models.medicaldevices.EKG;
+import models.medicaldevices.MedicalDevicesSimpleFactory;
+import models.medicaldevices.Thermometer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MedicalDevicesSimpleFactory simpleFactory = new MedicalDevicesSimpleFactory();
+
+        EKG ekg = (EKG) simpleFactory.getMedicalDevice(MedicalDeviceType.EKG, "ekg1");
+        ekg.collectData();
+
+        Thermometer thermometer = (Thermometer) simpleFactory.getMedicalDevice(MedicalDeviceType.THERMOMETER, "thermometer1");
+        thermometer.collectData();
+
+        BloodAnalyzer bloodAnalyzer = (BloodAnalyzer) simpleFactory.getMedicalDevice(MedicalDeviceType.BLOOD_ANALYZER, "bloodAnalyzer1");
+        bloodAnalyzer.collectData();
+
     }
 }
