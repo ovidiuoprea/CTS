9997ee4f53d3f241390a3aa60bde47d81ea43e11
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-08 12:08:33 +0300

    Tested Biological Sample Factory & Prototype

--- Subjects/BiotechnologyLaboratory/src/Main.java
+++ Subjects/BiotechnologyLaboratory/src/Main.java
@@ -1,5 +1,23 @@
+import enums.BiologicalSampleType;
+import models.factory.BiologicalSampleSimpleFactory;
+import models.factory.Virus;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BiologicalSampleSimpleFactory factory = new BiologicalSampleSimpleFactory();
+
+        Virus virus = (Virus) factory.createBiologicalSample(BiologicalSampleType.VIRUS, "1234", 22);
+        virus.processSample();
+
+        try {
+            Virus virusCopy = (Virus) virus.clone();
+            virusCopy.setIncubationTemperature(24);
+            virusCopy.processSample();
+            virusCopy.printReport();
+            virus.printReport();
+        }
+        catch (CloneNotSupportedException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
