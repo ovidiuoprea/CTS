e3baff611062cef41b6e28466cd79310dea72acb
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-08 17:38:01 +0300

    Create ElectricCar class

---
+++ Subjects/ElectricCarsTestingPlatform/src/models/builder/ElectricCar.java
@@ -0,0 +1,43 @@
+package models.builder;
+
+public class ElectricCar {
+    private String batteryType;
+    private int range;
+    private int numberOfEngines;
+    private String coolingSystem;
+    private boolean supportsFastCharging;
+    private boolean hasAutoPilot;
+    private boolean hasAdaptiveSuspension;
+
+    ElectricCar(String batteryType, int range, int numberOfEngines, String coolingSystem) {
+        this.batteryType = batteryType;
+        this.range = range;
+        this.numberOfEngines = numberOfEngines;
+        this.coolingSystem = coolingSystem;
+    }
+
+    void setSupportsFastCharging(boolean supportsFastCharging) {
+        this.supportsFastCharging = supportsFastCharging;
+    }
+
+    void setHasAutoPilot(boolean hasAutoPilot) {
+        this.hasAutoPilot = hasAutoPilot;
+    }
+
+    void setHasAdaptiveSuspension(boolean hasAdaptiveSuspension) {
+        this.hasAdaptiveSuspension = hasAdaptiveSuspension;
+    }
+
+    @Override
+    public String toString() {
+        return "ElectricCar{" +
+                "batteryType='" + batteryType + '\'' +
+                ", range=" + range +
+                ", numberOfEngines=" + numberOfEngines +
+                ", coolingSystem='" + coolingSystem + '\'' +
+                ", supportsFastCharging=" + supportsFastCharging +
+                ", hasAutoPilot=" + hasAutoPilot +
+                ", hasAdaptiveSuspension=" + hasAdaptiveSuspension +
+                '}';
+    }
+}
