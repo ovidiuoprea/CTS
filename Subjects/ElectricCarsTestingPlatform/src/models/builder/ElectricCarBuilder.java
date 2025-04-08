b248535181f5618ac0b3f2bb425437787b51909c
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-08 17:38:11 +0300

    Implemented ElectricCar builder

---
+++ Subjects/ElectricCarsTestingPlatform/src/models/builder/ElectricCarBuilder.java
@@ -0,0 +1,31 @@
+package models.builder;
+
+import interfaces.IElectricCarsBuilder;
+
+public class ElectricCarBuilder implements IElectricCarsBuilder {
+    private ElectricCar car;
+
+    public ElectricCarBuilder(String batteryType, int range, int numberOfEngines, String coolingSystem) {
+        car = new ElectricCar(batteryType, range, numberOfEngines, coolingSystem);
+    }
+
+    public ElectricCarBuilder addFastCharging() {
+        car.setSupportsFastCharging(true);
+        return this;
+    }
+
+    public ElectricCarBuilder addAdaptiveSuspension() {
+        car.setHasAdaptiveSuspension(true);
+        return this;
+    }
+
+    public ElectricCarBuilder addAutoPilot() {
+        car.setHasAutoPilot(true);
+        return this;
+    }
+
+    @Override
+    public ElectricCar build() {
+        return car;
+    }
+}
