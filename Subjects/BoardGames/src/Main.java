9dc81d683933359418b81bdeee20bc4f89f34955
ovidiuoprea <52795482+ovidiuoprea@users.noreply.github.com>
2025-04-08 22:55:12 +0300

    Tested Builder, Prototype, Prototype Factory

--- Subjects/BoardGames/src/Main.java
+++ Subjects/BoardGames/src/Main.java
@@ -1,8 +1,15 @@
+import enums.BoardGameType;
+import enums.ComponentType;
 import enums.PlatformServiceType;
 import models.builder.BoardGame;
 import models.builder.BoardGameBuilder;
+import models.builder.BoardGamePrototypeFactory;
+import models.factory.Board;
+import models.factory.ComponentFactory;
+import models.factory.Dice;
 import models.singleton.PlatformServiceManager;
 
+import java.awt.*;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -35,5 +42,43 @@ public class Main {
 
         System.out.println(boardGame);
 
+        ComponentFactory componentFactory = new ComponentFactory();
+        Dice dice = (Dice) componentFactory.getComponent(ComponentType.DICE, "Primul meu zar");
+        Board board = (Board) componentFactory.getComponent(ComponentType.BOARD, "Prima mea tabla");
+
+        System.out.println(dice + "\n" + board);
+
+        List<String> chessComponents = Stream.of(
+                "Pieces",
+                "Board"
+        ).collect(Collectors.toList());
+
+        BoardGame chess = new BoardGameBuilder(
+                "Chess",
+                2,
+                10,
+                chessComponents
+                ).build();
+        System.out.println("Original chess: " + chess);
+
+        try {
+            BoardGame chessClone = (BoardGame) chess.clone();
+            chessClone.addNewComponent("Dice");
+            System.out.println("Cloned chess: " + chessClone);
+            System.out.println("After changing the clone, the original chess: " + chess);
+
+            BoardGamePrototypeFactory boardGamePrototypeFactory = new BoardGamePrototypeFactory();
+            BoardGame diceCombat = boardGamePrototypeFactory.getBoardGame(BoardGameType.DICE_COMBAT);
+
+            System.out.println(diceCombat);
+
+            BoardGame tilePlacement = boardGamePrototypeFactory.getBoardGame(BoardGameType.TILE_PLACEMENT);
+        }
+        catch(CloneNotSupportedException exception) {
+            System.out.println("Could not clone game due to " + exception.getMessage());
+        }
+        catch(RuntimeException exception) {
+            System.out.println("Could not run game due to " + exception.getMessage());
+        }
     }
 }
